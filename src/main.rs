6ff9fc31cd76b442d368a67d5c8a5657cb36cc6d
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-02-20 19:32:21 -0800

    Refactor path code + write settings.json

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use color_eyre::eyre::{eyre, WrapErr};
 use procfs::net::TcpState;
 use std::{
+    convert::TryInto,
     env,
-    fs::File,
-    io::Write,
+    fs::{create_dir_all, write},
     path::PathBuf,
     process::{Command, Stdio},
     thread::sleep,
@@ -16,33 +16,44 @@ const CODE_SERVER_PORT: u16 = 8080;
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
+    let home: PathBuf = env::var_os("HOME")
+        .ok_or(eyre!("HOME var not set"))?
+        .try_into()
+        .wrap_err("$HOME is invalid")?;
+    let home = home
+        .canonicalize()
+        .wrap_err("Unable to canonicalize $HOME")?;
+    let xdg_config_home: PathBuf = env::var_os("XDG_CONFIG_HOME")
+        .map(|p| p.into())
+        .unwrap_or_else(|| home.clone().join(".config"));
+    let xdg_data_home: PathBuf = env::var_os("XDG_DATA_HOME")
+        .map(|p| p.into())
+        .unwrap_or_else(|| home.clone().join(".local").join("share"));
+
     let ssh_pubkey = env::var("SSH_PUBKEY").wrap_err("SSH_PUBKEY var invalid")?;
-    let home = env::var_os("HOME").ok_or(eyre!("HOME var not set"))?;
-    let auth_keys_file = [home.clone(), ".ssh".into(), "authorized_keys".into()]
-        .iter()
-        .collect::<PathBuf>();
-    std::fs::create_dir_all(auth_keys_file.parent().unwrap())?;
-    write!(File::create(auth_keys_file)?, "{}", ssh_pubkey)?;
+    let auth_keys_file = home.join(".ssh").join("authorized_keys");
+    create_dir_all(auth_keys_file.parent().unwrap())?;
+    write(auth_keys_file, ssh_pubkey)?;
 
     let code_server_password =
         env::var("CODE_SERVER_PASS").wrap_err("CODE_SERVER_PASS var invalid")?;
-    let xdg_config_home: PathBuf = env::var_os("XDG_CONFIG_HOME")
-        .map(|p| p.into())
-        .unwrap_or_else(|| [home, ".config".into()].iter().collect());
-    let code_server_config_path: PathBuf =
-        [xdg_config_home, "code-server".into(), "config.yaml".into()]
-            .iter()
-            .collect();
-    std::fs::create_dir_all(code_server_config_path.parent().unwrap())?;
-    write!(
-        File::create(code_server_config_path)?,
-        "bind-addr: 0.0.0.0:{}\n\
-        auth: password\n\
-        password: {}\n\
-        cert: false\n",
-        CODE_SERVER_PORT,
-        code_server_password
+    let code_server_config_path: PathBuf = xdg_config_home.join("code-server").join("config.yaml");
+    create_dir_all(code_server_config_path.parent().unwrap())?;
+    write(
+        code_server_config_path,
+        format!(
+            "bind-addr: 0.0.0.0:{}\n\
+            auth: password\n\
+            password: {}\n\
+            cert: false\n",
+            CODE_SERVER_PORT, code_server_password
+        ),
     )?;
+
+    let settings_json = include_bytes!("settings.json");
+    let settings_json_path = xdg_data_home.join("code-server");
+    create_dir_all(settings_json_path.parent().unwrap())?;
+    write(settings_json_path, settings_json)?;
 
     let status = Command::new("sudo")
         .arg("service")
